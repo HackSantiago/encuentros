8eec655de6707d45e71d5fb64d0e26f270b44075
Maximiliano Castro <max@axiomzen.co>
2016-05-07 16:54:26 -0300

    implements google maps in the home page and in the new page

--- public/js/home.js
+++ public/js/home.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
 
-  $map = $('#welcome-map');
-  if (!$map.length) {
+  var $map = document.getElementById('welcome-map');
+  if (!$map) {
     return ;
   }
 
-  var santiagoLocation = [-33.435960, -70.646306],
-      map = L.map('welcome-map').setView(santiagoLocation, 12);
+  var santiagoLocation = {lat: -33.435960, lng: -70.646306},
+      map = new google.maps.Map($map, {
+        center: santiagoLocation,
+        zoom: 12
+      });
 
-  L.tileLayer('https://api.mapbox.com/styles/v1/creedarky/cinxcugqq0000b1nub2sxmq2h/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY3JlZWRhcmt5IiwiYSI6ImNpbnhjdGZ6dTE3NHJ1YW0zb3Z4Z242M3QifQ.o0bJWPmReYxFiL1VVR_f3g', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 20
-  }).addTo(map);
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       function success(position) {
         var lat  = position.coords.latitude,
             lng = position.coords.longitude,
-            currentLocation = [lat, lng];
-        map.setView(currentLocation, 14);
+            currentLocation = {lat: lat, lng: lng};
+        map.setCenter(currentLocation);
+        map.setZoom(14);
       },
       function error(error) {
         console.log(error)
