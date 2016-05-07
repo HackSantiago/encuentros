a0eef1cb617d13477aa981fa5bcadd6f84d32ade
p1nox <pinox101@gmail.com>
2016-05-07 16:54:21 -0300

    geospatial and full text search indexes added

--- models/Event.js
+++ models/Event.js
@@ -1,26 +1,56 @@
+var R = require('ramda');
 var Promise = require('bluebird');
 var mongoose = require('mongoose');
+var searchPlugin = require('mongoose-search-plugin');
 
-/*
- Event
- title string
- desc string
- moderator -> fb id
- list of attendees -> fb id
- dates
- */
+
 var eventSchema = new mongoose.Schema({
-  title: String,
-  desc: String,
-  url: String,
-  moderator: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  title:        String,
+  desc:         String,
+  url:          String,
+  tags:         { type: [String] , default: '' },
+  moderator:    { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   participants: [ { type: mongoose.Schema.Types.ObjectId, ref: 'User' } ],
-  location: Array
-},
-  {
-    timestamps: true
+  location: {
+    type:  [Number],  // [<longitude>, <latitude>]
+    index: '2d'       // create the  geospatial index
   }
-);
+}, {
+  timestamps: true
+});
+
+eventSchema.plugin(searchPlugin, {
+  fields: ['title', 'desc', 'tags']
+});
+
+
+eventSchema.statics.findByText = function(text) {
+  if (!text) {
+    return Event.find({});
+  }
+
+  return Event.search(text);
+};
+
+
+eventSchema.statics.findNear = function(query) {
+  var params = R.pick([
+    'location', 'distance', 'limit'
+  ], query);
+
+  // we need to convert the distance to radians
+  // the raduis of Earth is approximately 6371 kilometers
+  var maxDistance = params.distance / 6371;
+
+  // find a location
+  return Event.findAsync({
+    location: {
+      $near: params.coords,
+      $maxDistance: maxDistance
+    }
+  });
+};
+
 
 var Model = mongoose.model('Event', eventSchema);
 module.exports = Event = Promise.promisifyAll(Model);
